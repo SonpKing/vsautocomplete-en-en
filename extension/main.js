8718924154bd588b251ba798a953cfb1784a1cf6
xiaobo <xbojin@gmail.com>
2021-01-19 01:11:41 +0800

    no hover

--- extension/main.js
+++ extension/main.js
@@ -106,36 +106,36 @@ function activate(context) {
 		}
 		));
 
-	subscriptions.push(
-		vscode.languages.registerHoverProvider(DOCUMENT_SELECTOR, {
-			provideHover: (document, position/*, token*/) => {
-				let beforeText = getTextBeforeCursor(document, position);
-				if (isCursorInTheString(beforeText)) return [];
-				let textAround = getTextAroundCursor(document, position);
-				if (!textAround) return null;
-				let strlen = textAround.length;
-				let item = findHintItem(textAround);
-				if (!item)  // 大写开头的查不到，可以变成小写再查一下
-					item = findHintItem(textAround.toLowerCase());
-				if (!item && textAround.charAt(strlen - 1) == "s")// 复数查不到，去掉s查一下
-					item = findHintItem(textAround.substring(0, strlen - 1));
-				if (!item && textAround.substring(strlen - 2, strlen) == "es")// 复数查不到，去掉es查一下
-					item = findHintItem(textAround.substring(0, strlen - 2));
-				if (!item && textAround.substring(strlen - 2, strlen) == "ed")// 过去式, end with ed
-				{
-					item = findHintItem(textAround.substring(0, strlen - 1)); // 过去式,has e, added d, 
-					if (!item) {
-						item = findHintItem(textAround.substring(0, strlen - 2)); //过去式, end with ed
-						if (!item)
-							item = findHintItem(textAround.substring(0, strlen - 3)); //过去式, 辅音字母结尾的
-					}
-				}
-				if (!item) return null;
-				return new vscode.Hover([
-					`**${item.name}**`, `*${item.set}*`, item.desc
-				]);
-			}
-		}));
+// 	subscriptions.push(
+// 		vscode.languages.registerHoverProvider(DOCUMENT_SELECTOR, {
+// 			provideHover: (document, position/*, token*/) => {
+// 				let beforeText = getTextBeforeCursor(document, position);
+// 				if (isCursorInTheString(beforeText)) return [];
+// 				let textAround = getTextAroundCursor(document, position);
+// 				if (!textAround) return null;
+// 				let strlen = textAround.length;
+// 				let item = findHintItem(textAround);
+// 				if (!item)  // 大写开头的查不到，可以变成小写再查一下
+// 					item = findHintItem(textAround.toLowerCase());
+// 				if (!item && textAround.charAt(strlen - 1) == "s")// 复数查不到，去掉s查一下
+// 					item = findHintItem(textAround.substring(0, strlen - 1));
+// 				if (!item && textAround.substring(strlen - 2, strlen) == "es")// 复数查不到，去掉es查一下
+// 					item = findHintItem(textAround.substring(0, strlen - 2));
+// 				if (!item && textAround.substring(strlen - 2, strlen) == "ed")// 过去式, end with ed
+// 				{
+// 					item = findHintItem(textAround.substring(0, strlen - 1)); // 过去式,has e, added d, 
+// 					if (!item) {
+// 						item = findHintItem(textAround.substring(0, strlen - 2)); //过去式, end with ed
+// 						if (!item)
+// 							item = findHintItem(textAround.substring(0, strlen - 3)); //过去式, 辅音字母结尾的
+// 					}
+// 				}
+// 				if (!item) return null;
+// 				return new vscode.Hover([
+// 					`**${item.name}**`, `*${item.set}*`, item.desc
+// 				]);
+// 			}
+// 		}));
 
 	// subscriptions.push(
 	// 	vscode.languages.registerSignatureHelpProvider(DOCUMENT_SELECTOR, {
